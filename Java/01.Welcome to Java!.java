78763cbccb9eb4f36757c75ea48de587bd1069a7
Haripriya-karunakaran03 <haripriya.karunakaran03@gmail.com>
2025-02-18 16:54:10 +0530

    Update 01.Welcome to Java!.java

--- Java/01.Welcome to Java!.java
+++ Java/01.Welcome to Java!.java
@@ -1,3 +1,22 @@
+/*
+Through this exercise, individuals will learn to set up their development environment, 
+write a simple Java program, understand the syntax required to display "Hello, World!" on the console. 
+This serves as a foundational step in their Java programming journey, enabling them to proceed with more complex challenges and projects on the HackerRank platform.
+
+Input Format:
+There is no input for this challenge.
+
+Output Format:
+
+You must print two lines of output:
+Print Hello, World. on the first line.
+Print Hello, Java. on the second line.
+
+Sample Output
+Hello, World.
+Hello, Java.
+*/
+
 public class Solution {
     public static void main(String[] args) {
         System.out.println("Hello, World.");
