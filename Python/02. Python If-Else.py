e5cdf9a587b57ce2720c60850b47629da248ae3b
Haripriya-karunakaran03 <haripriya.karunakaran03@gmail.com>
2025-02-19 16:02:58 +0530

    Create 02. Python If-Else.py

---
+++ Python/02. Python If-Else.py
@@ -0,0 +1,39 @@
+"""
+Task
+Given an integer, n, perform the following conditional actions:
+
+If n is odd, print Weird
+If n is even and in the inclusive range of 2 to 5, print Not Weird
+If n is even and in the inclusive range of 6 to 20, print Weird
+If n is even and greater than 20, print Not Weird
+"""
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    if(n % 2 != 0):
+        print("Weird")
+    else:
+        if(n >= 2 and n <= 5):
+            print("Not Weird")
+        elif(n >= 6 and n <= 20):
+            print("Weird")
+        else:
+            print("Not Weird")
+
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    mod = n % 2
+    
+    if(mod != 0):
+        print("Weird")
+    else:
+        if(2 <= n <= 5):
+            print("Not Weird")
+        elif(6 <= n <= 20):
+            print("Weird")
+        else:
+            print("Not Weird")
