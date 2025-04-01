04e134d471aaf1e3e46ddd3ebf5f199832943567
Haripriya-karunakaran03 <haripriya.karunakaran03@gmail.com>
2025-04-01 15:48:46 +0530

    Update 10. Java Static Initializer Block.java

--- Java/10. Java Static Initializer Block.java
+++ Java/10. Java Static Initializer Block.java
@@ -1,6 +1,8 @@
 // https://www.hackerrank.com/challenges/java-static-initializer-block/problem?isFullScreen=true
 
 //Static blocks executes only once when class gets loaded.
+
+//SOLUTION - 1
 import java.io.*;
 import java.util.*;
 import java.text.*;
@@ -31,6 +33,33 @@ public static void main(String[] args){
 		}
 	}
 }
+
+// SOLUTION - 2
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+    static Scanner sc = new Scanner(System.in);
+    static int B =sc.nextInt();
+    static int H = sc.nextInt();
+    static boolean flag = (B > 0 && H > 0);
+    
+    static{
+        if(!flag)
+	{
+        System.out.println("java.lang.Exception: Breadth and height must be positive");
+    	}
+    }
+public static void main(String[] args){
+		if(flag){
+			int area=B*H;
+			System.out.print(area);
+		}
+	}//end of main
+}//end of class
 
 
 
